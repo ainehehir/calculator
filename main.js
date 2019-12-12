28f69001f1b883370ff7379ac88fd0e34e5dbad3
ainehehir <ainehehir@gmail.com>
2019-12-12 15:22:15 +0000

    add calcuate function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
  const calculator = document.querySelector('.calculator');
  const display = document.querySelector('.calculator__display');
  const keys = calculator.querySelector('.calculator__keys');
-
+ 
 // listen for all key presses
 // determines what type of key is clicked based on the data-action attribute
 keys.addEventListener('click', e => {
@@ -10,13 +10,18 @@ keys.addEventListener('click', e => {
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+
+
+        Array.from(key.parentNode.children)
+        		.forEach( k => k.classList.remove('is-depressed'));
 
         if(!action){
 	// console.log('number key!');
 	// if calculator shows 0, we wnat to replace the  calculators display with the textContent of the 
 	// clicked key
 	// If the calculator shows a non-zero number, we want to append the clicked key to the displayed number
-	if(displayedNum === '0'){
+	if(displayedNum === '0' || previousKeyType === 'operator'){
 		display.textContent = keyContent;
 	} else {
 		display.textContent = displayedNum + keyContent;
@@ -29,7 +34,11 @@ if(
 	action === 'multiply' ||
 	action ==='divide')
 {
-	console.log('operator key!');
+	// console.log('operator key!');
+	key.classList.add('is-depressed');
+	calculator.dataset.previousKeyType = 'operator';
+	calculator.dataset.firstValue = displayedNum;
+	calculator.dataset.operator = action;
 }
 
 if(action === 'decimal'){
@@ -43,38 +52,33 @@ if(action === 'clear'){
 }
 
 if(action === 'calculate'){
-	console.log('equal key!');
+	const firstValue = calculator.dataset.firstValue;
+	const operator = calculator.dataset.operator;
+	const secondValue = displayedNum;
+
+	display.textContent = calculate(firstValue, operator, secondValue);
 }
 
 
 	}
 });
 
-// if there is no  data-action attribute, then it must be a number key
-// if(!action){
-// 	console.log('number key!');
-// }
 
-// if(
-// 	action === 'add' ||
-// 	action === 'subtract' ||
-// 	action === 'multiply' ||
-// 	action ==='divide')
-// {
-// 	console.log('operator key!');
-// }
+const calculate = (n1, operator, n2) =>{
 
-// if(action === 'decimal'){
-// 	console.log('decimal keys!')
-// }
+	let result = '';
 
-// if(action === 'clear'){
-// 	console.log('clear key!');
-// }
-
-// if(action === 'calculate'){
-// 	console.log('equal key!');
-// }
+	if(operator === 'add'){
+		result = parseFloat(n1) + parseFloat(n2);
+	} else if(operator === 'subtract'){
+		result = parseFloat(n1) - parseFloat(n2);
+	} else if(operator === 'multiply'){
+		result = parseFloat(n1) * parseFloat(n2);
+	} else if(operator === 'divide'){
+		result = parseFloat(n1)/parseFloat(n2);
+	}
+	return result;
+}
 
 
 
